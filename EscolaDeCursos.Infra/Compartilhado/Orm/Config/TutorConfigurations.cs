e2868df75d7258845381c860347fea4ac94e8a2f
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-07-09 18:43:56 -0300

    ADD DBSet Tutores in DBContext e TutorConfigurations

---
+++ EscolaDeCursos.Infra/Compartilhado/Orm/Config/TutorConfigurations.cs
@@ -0,0 +1,40 @@
+using EscolaDeCursos.Dominio.Modulos.ModuloTutor;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EscolaDeCursos.Infra.Compartilhado.Orm.Config;
+
+class TutorConfiguration : IEntityTypeConfiguration<Tutor>
+{
+    public void Configure(EntityTypeBuilder<Tutor> builder)
+    {
+        builder.ToTable("TBTutor");
+
+        builder.HasKey(t => t.Id)
+            .HasName("PK_TBTutor");
+
+        builder.Property(t => t.Id)
+            .ValueGeneratedNever();
+
+        builder.Property(t => t.Nome)
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(t => t.Telefone)
+            .HasMaxLength(20)
+            .IsRequired();
+
+        builder.Property(t => t.Cpf)
+            .HasMaxLength(11)
+            .IsRequired();
+
+        // Indice Unicos
+        builder.HasIndex(t => t.Telefone)
+            .IsUnique()
+            .HasDatabaseName("UQ_TBTutor_Telefone");
+
+        builder.HasIndex(t => t.Cpf)
+            .IsUnique()
+            .HasDatabaseName("UQ_TBTutor_Cpf");
+    }
+}
--- EscolaDeCursos.Infra/Compartilhado/Orm/EscolaDeCursosDbContext.cs
+++ EscolaDeCursos.Infra/Compartilhado/Orm/EscolaDeCursosDbContext.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using EscolaDeCursos.Dominio.Modulos.ModuloTutor;
 using Microsoft.EntityFrameworkCore;
 
 namespace EscolaDeCursos.Infra.Compartilhado.Orm;
@@ -6,10 +7,12 @@ namespace EscolaDeCursos.Infra.Compartilhado.Orm;
 public sealed class EscolaDeCursosDbContext(
     DbContextOptions<EscolaDeCursosDbContext> options) : DbContext(options)
 {
+    public DbSet<Tutor> Tutores => Set<Tutor>();
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         Assembly assembly = typeof(EscolaDeCursosDbContext).Assembly;
 
         modelBuilder.ApplyConfigurationsFromAssembly(assembly);
     }
+
 }
