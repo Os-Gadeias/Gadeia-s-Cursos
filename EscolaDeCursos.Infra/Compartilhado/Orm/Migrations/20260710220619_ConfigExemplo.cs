ba80b66577b3eaa75d23975032f88f545c685c10
thig0S_ <thiagokovalski5@gmail.com>
2026-07-10 19:12:34 -0300

    Criado migrations e snapshot dos tutores, alteração no placeholder do cadastro

---
+++ EscolaDeCursos.Infra/Compartilhado/Orm/Migrations/20260710220619_ConfigExemplo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EscolaDeCursos.Infra.Compartilhado.Orm.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigExemplo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TBCategoria",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Icon = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Titulo = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Cor = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBCategoria", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TBTutor",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Telefone = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Cpf = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBTutor", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "UQ_TBCategoria_Titulo",
+                table: "TBCategoria",
+                column: "Titulo",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "UQ_TBTutor_Cpf",
+                table: "TBTutor",
+                column: "Cpf",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "UQ_TBTutor_Telefone",
+                table: "TBTutor",
+                column: "Telefone",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TBCategoria");
+
+            migrationBuilder.DropTable(
+                name: "TBTutor");
+        }
+    }
+}
