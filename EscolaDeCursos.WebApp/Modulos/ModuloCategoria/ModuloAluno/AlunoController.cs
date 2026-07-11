832e0126d77535881914bb56534c66fbaf65716a
Victor Jeremias <victorvarelejeremias@gmail.com>
2026-07-11 17:58:23 -0300

    ADD AlunoController completo

--- EscolaDeCursos.WebApp/Modulos/ModuloCategoria/ModuloAluno/AlunoController.cs
+++ EscolaDeCursos.WebApp/Modulos/ModuloCategoria/ModuloAluno/AlunoController.cs
@@ -1,5 +1,7 @@
 using AutoMapper;
 using EscolaDeCursos.Aplicacao.Modulos.ModuloAluno;
+using EscolaDeCursos.WebApp.Compartilhado.Extensions;
+using FluentResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EscolaDeCursos.WebApp.Modulos.ModuloCategoria.ModuloAluno;
@@ -23,11 +25,74 @@ public class AlunoController : Controller
         return View(vms);
     }
 
-    [HttpPost]
+    [HttpGet]
     public ActionResult Cadastrar()
     {
         return View();
     }
 
+    [HttpPost]
+    public ActionResult Cadastrar(CadastrarAlunoViewModel vm)
+    {
+        CadastrarAlunoDto dto = mapper.Map<CadastrarAlunoDto>(vm);
 
+        Result result = servicoAluno.Cadastrar(dto);
+
+        if (result.IsFailed)
+        {
+            ModelState.AddModelError(result);
+            return View(vm);
+        }
+
+        return RedirectToAction(nameof(Listar));
+    }
+
+    [HttpGet]
+    public ActionResult Editar(Guid id)
+    {
+        ListarAlunoDto? dto = servicoAluno.SelecionarPorId(id);
+
+        EditarAlunoViewModel vm = mapper.Map<EditarAlunoViewModel>(dto);
+
+        return View(vm);
+    }
+
+    [HttpPost]
+    public ActionResult Editar(EditarAlunoViewModel vm)
+    {
+        if (ModelState.IsValid)
+            return View(vm);
+
+        EditarAlunoDto dto = mapper.Map<EditarAlunoDto>(vm);
+
+        Result result = servicoAluno.Editar(dto);
+
+        if (result.IsFailed)
+        {
+            ModelState.AddModelError(result);
+            return View(vm);
+        }
+
+        return RedirectToAction(nameof(Listar));
+    }
+
+    [HttpGet]
+    public ActionResult Excluir(Guid id)
+    {
+        ListarAlunoDto dto = servicoAluno.SelecionarPorId(id);
+
+        ExcluirAlunoViewModel vm = mapper.Map<ExcluirAlunoViewModel>(dto);
+
+        return View(vm);
+    }
+
+    [HttpPost]
+    public ActionResult Excluir(ExcluirAlunoViewModel vm)
+    {
+        ExcluirAlunoDto dto = mapper.Map<ExcluirAlunoDto>(vm);
+
+        servicoAluno.Excluir(dto);
+
+        return RedirectToAction(nameof(Listar));
+    }
 }
