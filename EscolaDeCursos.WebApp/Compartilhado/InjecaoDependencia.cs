3a2e0f42e88d385c0a91b57c89d1a5e077ad05b1
thig0S_ <thiagokovalski5@gmail.com>
2026-07-21 12:50:20 -0300

    Adicionado UserId a Identidade Base, criada interface e implementacao do UserProvider

--- EscolaDeCursos.WebApp/Compartilhado/InjecaoDependencia.cs
+++ EscolaDeCursos.WebApp/Compartilhado/InjecaoDependencia.cs
@@ -1,3 +1,5 @@
+using EscolaDeCursos.Dominio.Compartilhado.Identity;
+using EscolaDeCursos.WebApp.Compartilhado.Identity;
 using EscolaDeCursos.WebApp.Compartilhado.Mapping;
 using Microsoft.AspNetCore.Identity;
 
@@ -43,6 +45,9 @@ public static class InjecaoDependencia
             .RequireAuthenticatedUser()
             .Build();
         });
+
+        services.AddHttpContextAccessor();
+        services.AddScoped<IUserProvider, UserProvider>();
 
         services.AddAutoMapper(mapperConfig =>
         {
